e2a9b5376d39b7783f9cab632cfab6dc885ed3db
brocli <brocli@email.com>
2020-08-03 15:59:28 +0100

    support --production build args

--- utils/env.js
+++ utils/env.js
@@ -1,4 +1,9 @@
 // tiny wrapper with default env vars
+
+if (!process.env.NODE_ENV && process.argv.includes('--production')) {
+  process.argv.NODE_ENV = 'production'
+}
+
 module.exports = {
   NODE_ENV: (process.env.NODE_ENV || "development"),
   PORT: (process.env.PORT || 3000)
